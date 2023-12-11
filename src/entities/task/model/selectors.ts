b50567325af4e8cbf5d7d7fe6c79e98b2031771a
ThunderBoltCreator <lololol2763@gmail.com>
2023-12-11 16:46:37 +0300

    asddasgcx

--- src/entities/task/model/selectors.ts
+++ src/entities/task/model/selectors.ts
@@ -1,15 +1,11 @@
 import type { RootState } from "app/appStore.ts"
 
-export const selectTasks = (state: RootState) => state.tasks.items
-export const selectTasksByTodoId = (id: string) => (state: RootState) => state.tasks.items[id]
-export const selectTasksCount = (state: RootState) => state.tasks.totalCount
-
 export const TasksSelectors = {
   tasks(state: RootState) {
     return state.tasks.items
   },
-  taskById(id: string) {
-    return (state: RootState) => state.tasks.items[id]
+  taskById(id: string | undefined) {
+    return (state: RootState) => id ? state.tasks.items[id] : []
   },
   tasksCount(state: RootState) {
     return state.tasks.totalCount
--- src/pages/single-todolist/SingleTodoList.tsx
+++ src/pages/single-todolist/SingleTodoList.tsx
@@ -1,15 +1,12 @@
 import { useParams } from "react-router-dom"
 import { useAppSelector } from "shared/lib/ReduxHooks.ts"
-import { selectTodos } from "entities/todoLists/model/todosSlice.ts"
-import { selectTasksByTodoId } from "entities/task/model/selectors.ts"
+import { TasksSelectors} from "entities/task/model/selectors.ts"
 
 export function SingleTodoList() {
   const { id } = useParams<{ id: string }>()
 
-  const todo = useAppSelector(selectTodos)?.filter((el) => el.id === id)
-  const todo2 = useAppSelector(selectTasksByTodoId(id as string))
-  console.log("todo2", todo2)
-  const tasks = []
-  console.log(todo)
+  const tasks = useAppSelector(TasksSelectors.taskById(id))
+  console.log(tasks)
+
   return <div>ID: {id}</div>
 }
